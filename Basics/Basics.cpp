41bb7b3ed81f8dc111b3e77a589c53e24daf6d06
Hishita Gupta <159264060+HishitaGupta@users.noreply.github.com>
2025-04-28 13:36:15 +0530

    init

---
+++ Basics/Basics.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+
+// int main(){
+//     cout<<"Hello Hishita"<<endl;
+//     cout<<"Hello Hishita \n";
+//     string s;
+//     cin>>s;
+//     cout<<s;
+//     string y;
+//     getline(cin,y);
+//     cout<<y;
+   
+// }
+
+//int main(){
+//    int age;
+//    cout<<"Enter Age:";
+//    cin>>age;
+//
+//    if(age>=18){
+//        cout<<"Adult";
+//    }
+//    else if(age<18){
+//        cout<<"Not an adult";
+//    }
+//    else{
+//        cout<<"Wrong Input";
+//    }
+//}
+
+//Switch
+int main(){
+	int day;
+	cin>>day;
+	
+	switch(day){
+		case 1:
+			cout<<"Monday";
+			break;
+		case 2:
+			cout<<"Tuesday";
+			break;
+		case 3:
+			cout<<"Wednesday";
+			break;
+		default:
+			cout<<"Invalid Input";
+	}
+	
+}
+
