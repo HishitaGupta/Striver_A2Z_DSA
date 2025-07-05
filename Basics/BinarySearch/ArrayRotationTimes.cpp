ba3b72c3b1f742bba7220475f65352a7ccb36ab1
Hishita Gupta <159264060+HishitaGupta@users.noreply.github.com>
2025-07-05 14:43:07 +0530

    binary search practice

--- Basics/BinarySearch/ArrayRotationTimes.cpp
+++ Basics/BinarySearch/ArrayRotationTimes.cpp
@@ -17,82 +17,86 @@ using namespace std;
 //     cout<<d;
 // }
 
-void checkRotationTimesBS(vector<int> vec) {
-    int low=0; int high = vec.size()-1;
-    int minNum = INT_MAX;
-    int minIndex=-1;
+// void checkRotationTimesBS(vector<int> vec) {
+//     int low=0; int high = vec.size()-1;
+//     int minNum = INT_MAX;
+//     int minIndex=-1;
+//
+//     while (low<=high) {
+//         int mid =(low+high)/2;
+//
+//         if (vec[low]<=vec[high]) {
+//             minNum=min(minNum,vec[low]);
+//             minIndex=low;
+//             break;
+//         }
+//
+//         if (vec[low]<vec[mid]) {
+//             //left is sorted
+//             minNum = min(minNum,vec[low]);
+//             if (minNum==vec[low]) {
+//                 minIndex = low;
+//             }
+//             low=mid+1;
+//         }else {
+//             minNum = min(minNum,vec[mid]);
+//             if (minNum==vec[mid]) {
+//                 minIndex = mid;
+//             }
+//             high=mid-1;
+//         }
+//     }
+//
+//     cout<<minIndex<<minNum;
+//
+// }
+//
+// void  checkRotationTimesBSDuplicates(vector<int> vec) {
+//     int low=0; int high=vec.size()-1;
+//     int minNum = INT_MAX;
+//     int minIndex=-1;
+//
+//     while (low<=high) {
+//         int mid = (low+high)/2;
+//
+//         if (vec[low]==vec[mid] && vec[mid]==vec[high]) { //if duplicates
+//             low++; //skip duplicates
+//             high--;
+//             continue;
+//         }
+//
+//         if (vec[low]<vec[high]) { //if segment is sorted
+//             minIndex=low;
+//             break;
+//         }
+//
+//         if (vec[low]<=vec[mid]) {
+//             //left is sorted
+//             minNum = min(minNum,vec[low]);
+//             if (minNum==vec[low]) {
+//                 minIndex = low;
+//             }
+//             low=mid+1;
+//         }else {
+//             minNum = min(minNum,vec[mid]);
+//             if (minNum==vec[mid]) {
+//                 minIndex = mid;
+//             }
+//             high=mid;
+//         }
+//     }
+//
+//     cout<<minIndex;
+//
+// }
 
-    while (low<=high) {
-        int mid =(low+high)/2;
 
-        if (vec[low]<=vec[high]) {
-            minNum=min(minNum,vec[low]);
-            minIndex=low;
-            break;
-        }
 
-        if (vec[low]<vec[mid]) {
-            //left is sorted
-            minNum = min(minNum,vec[low]);
-            if (minNum==vec[low]) {
-                minIndex = low;
-            }
-            low=mid+1;
-        }else {
-            minNum = min(minNum,vec[mid]);
-            if (minNum==vec[mid]) {
-                minIndex = mid;
-            }
-            high=mid-1;
-        }
-    }
-
-    cout<<minIndex<<minNum;
-
-}
-
-void  checkRotationTimesBSDuplicates(vector<int> vec) {
-    int low=0; int high=vec.size()-1;
-    int minNum = INT_MAX;
-    int minIndex=-1;
-
-    while (low<=high) {
-        int mid = (low+high)/2;
-
-        if (vec[low]==vec[mid] && vec[mid]==vec[high]) { //if duplicates
-            low++; //skip duplicates
-            high--;
-            continue;
-        }
-
-        if (vec[low]<vec[high]) { //if segment is sorted
-            minIndex=low;
-            break;
-        }
-
-        if (vec[low]<=vec[mid]) {
-            //left is sorted
-            minNum = min(minNum,vec[low]);
-            if (minNum==vec[low]) {
-                minIndex = low;
-            }
-            low=mid+1;
-        }else {
-            minNum = min(minNum,vec[mid]);
-            if (minNum==vec[mid]) {
-                minIndex = mid;
-            }
-            high=mid;
-        }
-    }
-
-    cout<<minIndex;
-
-}
 
 int main(){
     vector<int> vec ={1,1,1,1,1};
     // checkRotationTimes(vec);
     // checkRotationTimesBS(vec);
-    checkRotationTimesBSDuplicates(vec);
+    // checkRotationTimesBSDuplicates(vec);
+    // checkRotationTimes_Practice(vec);
 }
--- Basics/BinarySearch/FirstLastCountOccurence.cpp
+++ Basics/BinarySearch/FirstLastCountOccurence.cpp
@@ -61,9 +61,71 @@ void firstLastOccurence_BS(vector<int>vec,int target) {
     }
 }
 
+pair<int ,int> findLowerBound(vector<int>vec,int target) {
+    int low=0; int high=vec.size()-1;
+    int ans =-1;
+
+    while (low<=high) {
+        int mid =(low+high)/2;
+
+        if (vec[mid]>=target) {
+            ans=mid;
+            high=mid-1; //to find smallest
+        }else {
+            low=mid+1;
+        }
+    }
+
+    if (ans==-1) return {-1,-1};
+
+    return {vec[ans],ans};
+}
+
+int findUpperBound(vector<int>vec,int target) {
+    int low=0; int high=vec.size()-1;
+    int ans =-1;
+
+    while (low<=high) {
+        int mid =(low+high)/2;
+
+        if (vec[mid]>target) {
+            ans=mid;
+            high=mid-1; //to find smallest
+        }else {
+            low=mid+1;
+        }
+    }
+
+    return ans;
+}
+
+void firstLastOccurence_Practice(vector<int> vec,int target) {
+    //find lower bound and upper bound
+    //lowerbound=smallest index of that element or element just above it
+    //upperbound = smallest index of element just above it
+
+    pair<int,int> lowerBound ;
+    int upperBound =-1;
+
+    lowerBound = findLowerBound(vec,target);
+
+    cout<<lowerBound.first;
+
+    if (lowerBound.second==-1 || lowerBound.first!=target) {
+        cout<<"{-1,-1}"<<endl;
+        return;
+    }
+    upperBound = findUpperBound(vec,target);
+
+    cout<<"First Occurence:"<<lowerBound.second<<endl;
+    cout<<"Last Occurence:"<<upperBound-1<<endl;
+}
+
 int main() {
     vector<int>vec ={1,1,1,1,2,4,5,6};
     // firstLastOccurence_Brute(vec,1);
-    firstLastOccurence_BS(vec,1);
+    // firstLastOccurence_BS(vec,1);
     // firstLastoccurence(vec,1); -> using upperbound and lowerbound
+
+    firstLastOccurence_Practice(vec,1); //-> using upperbound and lowerbound
 }
