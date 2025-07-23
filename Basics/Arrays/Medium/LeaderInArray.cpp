a4fadd7bc8442d73131d5a8f4bb17ea5bcdaac5d
Hishita Gupta <159264060+HishitaGupta@users.noreply.github.com>
2025-07-23 11:15:32 +0530

    array medium revision

--- Basics/Arrays/Medium/LeaderInArray.cpp
+++ Basics/Arrays/Medium/LeaderInArray.cpp
@@ -21,10 +21,44 @@ void leaderInArray(vector<int>vec) {
     }
 }
 
+int leaderInArrayBrute(vector<int> vec){
+    int leader =-1;
+
+    for (int i=0;i<vec.size();i++){
+        for (int j=i+1;j<vec.size();j++){
+            leader = vec[i];
+            if (vec[i]<vec[j]) {
+                leader=-1;
+                break;
+            }
+        }
+        if (leader!=-1)  return leader;
+    }
+
+    return leader;
+}
+
+void leaderInArrayOptimal(vector<int> vec) {
+    int max= INT_MIN;
+    vector<int>ans;
+    for (int i=vec.size()-1;i>=0;i--) {
+        if (vec[i]>max) {
+            max=vec[i];
+            ans.push_back(vec[i]);
+        }
+    }
+    for (auto it : ans) {
+        cout<<it<<",";
+    }
+}
+
+
 int main(){
 
-    vector<int> vec={1,2,3,2};
+    vector<int> vec={1,2,3,0,0,0};
 
-    leaderInArray(vec);
+    // leaderInArray(vec);
+    // cout<<leaderInArrayBrute(vec);
+    leaderInArrayOptimal(vec);
 
 }
