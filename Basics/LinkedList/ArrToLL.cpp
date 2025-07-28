363c4a0fd446955ffb24a02ae437f825fa3baad4
Hishita Gupta <159264060+HishitaGupta@users.noreply.github.com>
2025-07-28 16:12:59 +0530

    Singly Linkedlist

--- Basics/LinkedList/ArrToLL.cpp
+++ Basics/LinkedList/ArrToLL.cpp
@@ -7,6 +7,8 @@ public:
     int data;
     Node* next;
 
+
+public:
     Node (int data,Node* next) {
         this->data = data;
         this->next =next;
@@ -128,6 +130,81 @@ Node* removeByValue(Node* head,int k) {
     return head;
 }
 
+Node* insertHead(Node* head,int data) {
+    Node* temp= new Node (data);
+    temp->next = head;
+    head=temp;
+
+    return head;
+}
+
+Node* insertTail(Node* head,int data) {
+
+    if (head==NULL) return new Node (data,head);
+
+    Node* temp= head;
+
+    while (temp->next != NULL) {
+        temp=temp->next;
+    }
+
+    Node* Tail= new Node (data);
+    temp->next = Tail;
+
+    return head;
+}
+
+Node* insertAtKthPlace(Node* head, int k,int data) {
+
+    if (head==NULL) {
+        return new Node (data,head);
+    }
+
+    if (k==1) return insertHead(head,data);
+
+    int count=0;
+    Node* temp = head;
+    Node* prev= NULL ;
+
+
+
+
+    while (temp != NULL) {
+        count++;
+        if (count==k-1) {
+            Node* newNode = new Node(data);
+            newNode->next = temp->next;
+            temp->next=newNode;
+            break;
+        }
+        temp = temp->next;
+    }
+
+    return head;
+}
+
+Node* insertBeforeValueK(Node* head, int value,int valueToInsert) {
+    if (head==NULL) {
+        return NULL;
+    }
+
+    if (head->data == value) {
+        return new Node(valueToInsert,head);
+    }
+
+    Node* temp = head;
+
+    while (temp->next != NULL) {
+        if (temp->next->data ==value) {
+            Node* newNode = new Node(valueToInsert,temp->next);
+            temp->next=newNode;
+            break;
+        }
+        temp= temp->next;
+    }
+
+    return head;
+}
 
 int main(){
     vector<int> arr = {1,2,3,4,5,6,7,8};
@@ -137,6 +214,10 @@ int main(){
     head=removeTail(head);
     head=removeKthElement(head,7);
     head=removeByValue(head,2);
+    head=insertHead(head,1);
+    head=insertTail(head,8);
+    head=insertAtKthPlace(head,1,0);
+    head=insertBeforeValueK(head,6,21);
 
     traverseLL(head);
 
